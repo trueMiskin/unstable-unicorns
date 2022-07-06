39e2837a66e4587eb3c2072544d0167dcb88c5cc
Michal Kodad <misa.kodad@gmail.com>
2022-07-06 23:56:41 +0200

    Added conditional effect

---
+++ UnstableUnicorn/UnstableUnicornCore/BasicEffects/ConditionalEffect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnstableUnicornCore.BasicEffects {
+    class ConditionalEffect : AEffect {
+        private AEffect _condition;
+        private AEffect _thenEffect;
+        public ConditionalEffect(Card owningCard, AEffect condition, AEffect thenEffect)
+            : base(owningCard, 0 /* For this effect is not needed*/) {
+            _condition = condition;
+            _thenEffect = thenEffect;
+        }
+
+        public override void ChooseTargets(GameController gameController) {
+            // choosing targets only for condition
+            // the then effect will be resolved in next chain link
+            _condition.ChooseTargets(gameController);
+        }
+
+        public override void InvokeEffect(ETriggerSource triggerSource, AEffect? effect, GameController gameController) {
+            _condition.InvokeEffect(triggerSource, effect, gameController);
+            gameController.AddNewEffectToChainLink(_thenEffect);
+        }
+
+        public override bool MeetsRequirementsToPlay(GameController gameController) {
+            return _condition.MeetsRequirementsToPlayInner(gameController);
+        }
+
+        public override bool MeetsRequirementsToPlayInner(GameController gameController) => MeetsRequirementsToPlay(gameController);
+    }
+}
