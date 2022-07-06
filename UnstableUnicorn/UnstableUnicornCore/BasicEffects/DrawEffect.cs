746c7ee4414b3c760173e1ca8d102179e34b4940
Michal Kodad <misa.kodad@gmail.com>
2022-07-07 00:50:03 +0200

    Added draw effect

---
+++ UnstableUnicorn/UnstableUnicornCore/BasicEffects/DrawEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnstableUnicornCore.BasicEffects {
+    public class DrawEffect : AEffect {
+        public DrawEffect(Card owningCard, int cardCount) : base(owningCard, cardCount) {}
+
+        public override void ChooseTargets(GameController gameController) {}
+
+        public override void InvokeEffect(ETriggerSource triggerSource, AEffect? effect, GameController gameController) {
+            for (int i = 0; i < _cardCount; i++)
+                gameController.PlayerDrawCard(OwningCard.Player);
+        }
+
+        public override bool MeetsRequirementsToPlayInner(GameController gameController) => true;
+    }
+}
