8dcf48fc6bda79b6924b816637deede7c5f763ae
Michal Kodad <misa.kodad@gmail.com>
2022-07-07 01:14:22 +0200

    Added and effect

---
+++ UnstableUnicorn/UnstableUnicornCore/BasicEffects/AndEffect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnstableUnicornCore.BasicEffects {
+    public class AndEffect : AEffect {
+        private AEffect _firstEffect, _secondEffect;
+        public AndEffect(Card owningCard, AEffect firstEffect, AEffect secondEffect)
+            : base(owningCard, 0 /* For this effect is not needed*/) {
+            _firstEffect = firstEffect;
+            _secondEffect = secondEffect;
+        }
+
+        public override void ChooseTargets(GameController gameController) {
+            _firstEffect.ChooseTargets(gameController);
+            _secondEffect.ChooseTargets(gameController);
+        }
+
+        public override void InvokeEffect(ETriggerSource triggerSource, AEffect? effect, GameController gameController) {
+            _firstEffect.InvokeEffect(triggerSource, effect, gameController);
+            _secondEffect.InvokeEffect(triggerSource, effect, gameController);
+        }
+
+        public override bool MeetsRequirementsToPlayInner(GameController gameController) {
+            return _firstEffect.MeetsRequirementsToPlayInner(gameController) &&
+                _secondEffect.MeetsRequirementsToPlayInner(gameController);
+        }
+    }
+}
