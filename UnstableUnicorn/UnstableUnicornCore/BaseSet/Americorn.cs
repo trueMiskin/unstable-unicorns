c7b8a1791c56071ff482f862a2f5b23635715fd9
Michal Kodad <misa.kodad@gmail.com>
2022-07-09 18:10:56 +0200

    Added new Card - Americorn + test

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/Americorn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class Americorn : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Americorn")
+                .CardType(ECardType.Unicorn)
+                .Text("When this card enters your Stable, choose any player. Pull a card from that player's hand.")
+                .TriggerEffect(
+                    (Card owningCard) =>
+                    new TriggerEffect(
+                        owningCard,
+                        (AEffect? affect, Card? card) => owningCard == card,
+                        new List<ETriggerSource>() { ETriggerSource.CardEnteredStable },
+                        (Card _, GameController gameController) => new PullOpponentsCardEffect(owningCard, cardCount: 1,
+                                                                                               numberSelectedPlayers: 1)
+                    )
+                );
+        }
+    }
+}
---
+++ UnstableUnicorn/UnstableUnicornCoreTest/BaseSet/AmericornTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnstableUnicornCore;
+using UnstableUnicornCore.BaseSet;
+using Xunit;
+
+namespace UnstableUnicornCoreTest.BaseSet {
+    public class AmericornTest {
+        [Fact]
+        public void testFunctionality() {
+            SimplePlayerMockUp playerOne = new(), playerTwo = new();
+            BasicUnicorn basicUnicornTemplate = new();
+            GameController controller = new GameController(new List<Card>(), new List<Card>(), new List<APlayer>() { playerOne, playerTwo });
+
+            // protection before shuffling
+            Card basicUnicorn = basicUnicornTemplate.GetCardTemplate().CreateCard();
+            controller.Pile.Add(basicUnicorn);
+            Card americorn = new Americorn().GetCardTemplate().CreateCard();
+            controller.Pile.Add(americorn);
+
+            controller.PlayerDrawCard(playerOne);
+            controller.PlayerDrawCard(playerTwo);
+
+            Assert.Equal(playerOne.Hand[0], americorn);
+            Assert.Equal(playerTwo.Hand[0], basicUnicorn);
+
+            americorn.CardPlayed(controller, playerOne);
+
+            typeof(GameController).GetMethod("ResolveChainLink", BindingFlags.Instance | BindingFlags.NonPublic)
+                .Invoke(controller, new object[] { });
+            
+            Assert.Single(playerOne.Hand);
+            Assert.Empty(playerTwo.Hand);
+        }
+    }
+}
