7864ae4978bd9529bb48b7aa5cb945f6d2d133e2
Michal Kodad <misa.kodad@gmail.com>
2022-07-11 01:09:08 +0200

    Added Alluring Narwhal card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/AlluringNarwhal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class AlluringNarwhal : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Alluring Narwhal")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, you may STEAL an Upgrade card.")
+                .TriggerEffect((Card owningCard) =>
+                    new TriggerEffect(owningCard,
+                        (AEffect? affect, Card? card) => owningCard == card,
+                        new List<ETriggerSource>() { ETriggerSource.CardEnteredStable },
+                        (Card _) => new StealEffect(owningCard, 1, new List<ECardType>{ ECardType.Upgrade })
+                    )
+                );
+        }
+    }
+}
---
+++ UnstableUnicorn/UnstableUnicornCoreTest/BaseSet/AlluringNarwhalTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore;
+using UnstableUnicornCore.BaseSet;
+using Xunit;
+
+namespace UnstableUnicornCoreTest.BaseSet {
+    public class AlluringNarwhalTest {
+        [Fact]
+        public void testWithoutValidTarget() {
+            SimplePlayerMockUp playerOne = new(), playerTwo = new();
+            GameController controller = new GameController(new List<Card>(), new List<Card>(), new List<APlayer>() { playerOne, playerTwo });
+
+            // protection before shuffling
+            Card basicUnicorn = new BasicUnicorn().GetCardTemplate().CreateCard();
+            controller.Pile.Add(basicUnicorn);
+            Card alluringNarwhal = new AlluringNarwhal().GetCardTemplate().CreateCard();
+            controller.Pile.Add(alluringNarwhal);
+
+            controller.PlayerDrawCard(playerTwo);
+            controller.PlayerDrawCard(playerOne);
+
+            Assert.Equal(playerOne.Hand[0], basicUnicorn);
+            Assert.Equal(playerTwo.Hand[0], alluringNarwhal);
+
+            controller.PlayCardAndResolveChainLink(basicUnicorn, playerOne);
+            controller.PlayCardAndResolveChainLink(alluringNarwhal, playerTwo);
+
+            Assert.Empty(playerOne.Hand);
+            Assert.Single(playerOne.Stable);
+            Assert.Empty(playerOne.Upgrades);
+            Assert.Empty(playerOne.Downgrades);
+
+            Assert.Empty(playerTwo.Hand);
+            Assert.Single(playerTwo.Stable);
+            Assert.Empty(playerTwo.Upgrades);
+            Assert.Empty(playerTwo.Downgrades);
+        }
+
+        [Fact]
+        public void testWithValidTarget() {
+            SimplePlayerMockUp playerOne = new(), playerTwo = new();
+            GameController controller = new GameController(new List<Card>(), new List<Card>(), new List<APlayer>() { playerOne, playerTwo });
+
+            // protection before shuffling
+            Card rainbowAura = new RainbowAura().GetCardTemplate().CreateCard();
+            controller.Pile.Add(rainbowAura);
+            Card alluringNarwhal = new AlluringNarwhal().GetCardTemplate().CreateCard();
+            controller.Pile.Add(alluringNarwhal);
+
+            controller.PlayerDrawCard(playerTwo);
+            controller.PlayerDrawCard(playerOne);
+
+            Assert.Equal(playerOne.Hand[0], rainbowAura);
+            Assert.Equal(playerTwo.Hand[0], alluringNarwhal);
+
+            controller.PlayCardAndResolveChainLink(rainbowAura, playerOne);
+            controller.PlayCardAndResolveChainLink(alluringNarwhal, playerTwo);
+
+            Assert.Empty(playerOne.Hand);
+            Assert.Empty(playerOne.Stable);
+            Assert.Empty(playerOne.Upgrades);
+            Assert.Empty(playerOne.Downgrades);
+
+            Assert.Empty(playerTwo.Hand);
+            Assert.Single(playerTwo.Stable);
+            Assert.Single(playerTwo.Upgrades);
+            Assert.Empty(playerTwo.Downgrades);
+        }
+    }
+}
