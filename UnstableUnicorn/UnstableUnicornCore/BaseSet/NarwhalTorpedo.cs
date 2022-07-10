f6430381d361398f9c713b27fc80c9c4cd483bce
Michal Kodad <misa.kodad@gmail.com>
2022-07-11 00:04:45 +0200

    Added Narwhal Torpedo card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/NarwhalTorpedo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class NarwhalTorpedo : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Narwhal Torpedo")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, SACRIFICE all Downgrade cards.")
+                .TriggerEffect((Card owningCard) =>
+                    new TriggerEffect(
+                        owningCard,
+                        (AEffect? affect, Card? card) => owningCard == card,
+                        new List<ETriggerSource>() { ETriggerSource.CardEnteredStable },
+                        (Card _, GameController gameController) => new SacrificeEffect(owningCard, Int32.MaxValue, new List<ECardType>() { ECardType.Downgrade })
+                ));
+        }
+    }
+}
---
+++ UnstableUnicorn/UnstableUnicornCoreTest/BaseSet/NarwhalTorpedoTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore;
+using UnstableUnicornCore.BaseSet;
+using Xunit;
+
+namespace UnstableUnicornCoreTest.BaseSet {
+    public class NarwhalTorpedoTest {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(10)]
+        public void testOwnerPlayerCanPlayBasicUnicorn(int numberDowngrades) {
+            SimplePlayerMockUp playerOne = new(), playerTwo = new();
+            Pandamonium pandamoniumTemplate = new();
+            GameController controller = new GameController(new List<Card>(), new List<Card>(), new List<APlayer>() { playerOne, playerTwo });
+
+            // protection before shuffling
+            Card pandamoniumModel = pandamoniumTemplate.GetCardTemplate().CreateCard();
+            
+            for (int i = 0; i < numberDowngrades; i++)
+                controller.Pile.Add(pandamoniumTemplate.GetCardTemplate().CreateCard());
+            Card narwhalTorpedo = new NarwhalTorpedo().GetCardTemplate().CreateCard();
+            controller.Pile.Add(narwhalTorpedo);
+            
+            controller.PlayerDrawCard(playerTwo);
+            for (int i = 0; i < numberDowngrades; i++)
+                controller.PlayerDrawCard(playerOne);
+
+            Assert.Equal(playerTwo.Hand[0], narwhalTorpedo);
+            Assert.Equal(numberDowngrades, playerOne.Hand.Count);
+            for (int i = 0; i < numberDowngrades; i++)
+                Assert.Equal(playerOne.Hand[0].Name, pandamoniumModel.Name);
+
+            for (int i = 0; i < numberDowngrades; i++)
+                controller.PlayCardAndResolveChainLink(playerOne.Hand[0], playerTwo);
+            controller.PlayCardAndResolveChainLink(narwhalTorpedo, playerTwo);
+
+            Assert.Empty(playerOne.Hand);
+            Assert.Empty(playerOne.Stable);
+            Assert.Empty(playerOne.Upgrades);
+            Assert.Empty(playerOne.Downgrades);
+
+            Assert.Empty(playerTwo.Hand);
+            Assert.Single(playerTwo.Stable);
+            Assert.Equal(narwhalTorpedo, playerTwo.Stable[0]);
+            Assert.Empty(playerTwo.Upgrades);
+            Assert.Empty(playerTwo.Downgrades);
+
+            Assert.Equal(numberDowngrades, controller.DiscardPile.Count);
+        }
+    }
+}
