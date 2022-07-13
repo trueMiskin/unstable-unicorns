611e7d0485ca815f04d69bfc051a3f9b11c9fdba
Michal Kodad <misa.kodad@gmail.com>
2022-07-14 01:39:59 +0200

    Added Mermaid Unicorn card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/MermaidUnicorn.cs
@@ -0,0 +1,16 @@
+﻿using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class MermaidUnicorn : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Mermaid Unicorn")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, you may choose any player. Return a card in that player's Stable to their hand.")
+                .Cast((Card owningCard) => new ActivatableEffect(owningCard,
+                        (_) => new ReturnEffect(owningCard, 1, ECardTypeUtils.CardTarget)
+                    )
+                );
+        }
+    }
+}
