2a1e40783970bc1bdb6cd399c2c8d71513cad576
Michal Kodad <misa.kodad@gmail.com>
2022-07-14 00:25:42 +0200

    Added Good Deal card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/GoodDeal.cs
@@ -0,0 +1,17 @@
+﻿using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class GoodDeal : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Good Deal")
+                .CardType(ECardType.Spell)
+                .Text("DRAW 3 cards and DISCARD a card.")
+                .Cast((Card owningCard) => new AndEffect(owningCard,
+                        new DrawEffect(owningCard, 3),
+                        new DiscardEffect(owningCard, 1, ECardTypeUtils.CardTarget, PlayerTargeting.PlayerOwner)
+                    )
+                );
+        }
+    }
+}
