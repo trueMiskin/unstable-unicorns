364c9d11a161c8ca7e54c003666c4fefcf2d8ab4
Michal Kodad <misa.kodad@gmail.com>
2022-07-14 00:30:36 +0200

    Added Greedy Flying Unicorn card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/GreedyFlyingUnicorn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class GreedyFlyingUnicorn : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Greedy Flying Unicorn")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, DRAW a card. If this card would be sacrificed or destroyed, return it to your hand instead.")
+                .Cast((Card owningCard) => new DrawEffect(owningCard, 1))
+                .TriggerEffect(
+                    TriggerPredicates.IfThisCardWouldBeSacrificedOrDestroyed,
+                    new List<ETriggerSource> { ETriggerSource.ChangeLocationOfCard },
+                    (Card owningCard) => new ReturnThisCardToHand(owningCard)
+                );
+        }
+    }
+}
