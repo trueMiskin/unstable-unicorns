7148883ca8d50f5c9504fd3a13966eb9a8ba16cd
Michal Kodad <misa.kodad@gmail.com>
2022-07-14 16:10:20 +0200

    Added Rainbow Unicorn card + tests

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/RainbowUnicorn.cs
@@ -0,0 +1,17 @@
+﻿using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class RainbowUnicorn : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Rainbow Unicorn")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, you may bring a Basic Unicorn card from your hand directly into your Stable.")
+                .Cast(
+                    (Card owningCard) => new ActivatableEffect(owningCard,
+                        (_) => new BringCardFromHandOnTable(owningCard, 1, card => card.CardType == ECardType.BasicUnicorn)
+                    )
+                );
+        }
+    }
+}
---
+++ UnstableUnicorn/UnstableUnicornCoreTest/BaseSet/RainbowUnicornTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore;
+using UnstableUnicornCore.BaseSet;
+using Xunit;
+
+namespace UnstableUnicornCoreTest.BaseSet {
+    public class RainbowUnicornTest {
+        [Fact]
+        public void TestBringEffectWithoutValidTarget() {
+            SimplePlayerMockUp playerOne = new(), playerTwo = new();
+
+            GameController controller = new GameController(new List<Card>(), new List<Card>(), new List<APlayer>() { playerOne, playerTwo });
+
+            // protection before shuffling
+            Card americorn = new Americorn().GetCardTemplate().CreateCard();
+            controller.Pile.Add(americorn);
+            Card rainbowMane = new RainbowMane().GetCardTemplate().CreateCard();
+            controller.Pile.Add(rainbowMane);
+            Card brokenStable = new BrokenStable().GetCardTemplate().CreateCard();
+            controller.Pile.Add(brokenStable);
+            Card rainbowUnicorn = new RainbowUnicorn().GetCardTemplate().CreateCard();
+            controller.Pile.Add(rainbowUnicorn);
+
+            controller.PlayerDrawCard(playerOne);
+            controller.PlayerDrawCard(playerOne);
+            controller.PlayerDrawCard(playerOne);
+            controller.PlayerDrawCard(playerOne);
+
+            TestUtils.CheckPlayerPileSizes(playerOne, handSize: 4, stableSize: 0, numUpgrades: 0, numDowngrades: 0);
+            TestUtils.CheckPlayerPileSizes(playerTwo, handSize: 0, stableSize: 0, numUpgrades: 0, numDowngrades: 0);
+
+            controller.PlayCardAndResolveChainLink(rainbowUnicorn, playerOne);
+
+            // no valid target for rainbow unicorn
+            TestUtils.CheckPlayerPileSizes(playerOne, handSize: 3, stableSize: 1, numUpgrades: 0, numDowngrades: 0);
+            TestUtils.CheckPlayerPileSizes(playerTwo, handSize: 0, stableSize: 0, numUpgrades: 0, numDowngrades: 0);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(5)]
+        public void TestBringEffectWithValidTarget(int numberBasicUnicorns) {
+            SimplePlayerMockUp playerOne = new(), playerTwo = new();
+
+            GameController controller = new GameController(new List<Card>(), new List<Card>(), new List<APlayer>() { playerOne, playerTwo });
+
+            // protection before shuffling
+            for (int i = 0; i < numberBasicUnicorns; i++) {
+                Card basicUnicorn = new BasicUnicorn().GetCardTemplate().CreateCard();
+                controller.Pile.Add(basicUnicorn);
+                controller.PlayerDrawCard(playerOne);
+            }
+
+            Card americorn = new Americorn().GetCardTemplate().CreateCard();
+            controller.Pile.Add(americorn);
+            Card rainbowMane = new RainbowMane().GetCardTemplate().CreateCard();
+            controller.Pile.Add(rainbowMane);
+            Card brokenStable = new BrokenStable().GetCardTemplate().CreateCard();
+            controller.Pile.Add(brokenStable);
+            Card rainbowUnicorn = new RainbowUnicorn().GetCardTemplate().CreateCard();
+            controller.Pile.Add(rainbowUnicorn);
+
+            controller.PlayerDrawCard(playerOne);
+            controller.PlayerDrawCard(playerOne);
+            controller.PlayerDrawCard(playerOne);
+            controller.PlayerDrawCard(playerOne);
+
+            TestUtils.CheckPlayerPileSizes(playerOne, handSize: 4 + numberBasicUnicorns, stableSize: 0, numUpgrades: 0, numDowngrades: 0);
+            TestUtils.CheckPlayerPileSizes(playerTwo, handSize: 0, stableSize: 0, numUpgrades: 0, numDowngrades: 0);
+
+            controller.PlayCardAndResolveChainLink(rainbowUnicorn, playerOne);
+
+            TestUtils.CheckPlayerPileSizes(playerOne, handSize: 3 + numberBasicUnicorns - 1, stableSize: 2, numUpgrades: 0, numDowngrades: 0);
+            TestUtils.CheckPlayerPileSizes(playerTwo, handSize: 0, stableSize: 0, numUpgrades: 0, numDowngrades: 0);
+        }
+    }
+}
