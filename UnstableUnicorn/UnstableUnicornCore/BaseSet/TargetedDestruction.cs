85bcdf66d2849f876d0fb7fefc75032736382585
Michal Kodad <misa.kodad@gmail.com>
2022-07-14 22:53:44 +0200

    Added Targeted Destruction card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/TargetedDestruction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class TargetedDestruction : CardTemplateSource {
+        /// <summary>
+        /// Same as Chainsaw Unicorn
+        /// -> second edition text used
+        /// </summary>
+        /// <returns></returns>
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Targeted Destruction")
+                .CardType(ECardType.Spell)
+                .Text("DESTROY an Upgrade card or SACRIFICE a Downgrade card.")
+                .Cast((Card owningCard) => new ActivatableEffect(owningCard,
+                   new ChooseEffect(owningCard,
+                       new List<AEffect> {
+                            new DestroyEffect(owningCard, 1, new List<ECardType>{ECardType.Upgrade}),
+                            new SacrificeEffect(owningCard, 1, new List<ECardType>{ECardType.Downgrade})
+                       }
+                   ))
+                );
+        }
+    }
+}
