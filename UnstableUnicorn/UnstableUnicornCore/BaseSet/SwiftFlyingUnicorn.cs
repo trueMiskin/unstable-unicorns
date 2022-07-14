7d48517160c1fb8aa90375fc2da0f77c126ac8a1
Michal Kodad <misa.kodad@gmail.com>
2022-07-14 22:47:15 +0200

    Added Swift Flying Unicorn card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/SwiftFlyingUnicorn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class SwiftFlyingUnicorn : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Swift Flying Unicorn")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, you may choose an Instant card from the discard pile and add it to your hand. If this card would be sacrificed or destroyed, return it to your hand instead.")
+                .Cast((Card owningCard) => new ActivatableEffect(owningCard,
+                        new SearchDeckEffect(owningCard, 1, CardLocation.DiscardPile, (Card card) => card.CardType == ECardType.Instant)
+                    )
+                )
+                .TriggerEffect(
+                    TriggerPredicates.IfThisCardWouldBeSacrificedOrDestroyed,
+                    new List<ETriggerSource> { ETriggerSource.ChangeLocationOfCard },
+                    (Card owningCard) => new ReturnThisCardToHand(owningCard)
+                );
+        }
+    }
+}
