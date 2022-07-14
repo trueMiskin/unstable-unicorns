68f4b48abdd4c148c2c0cc0e79a9841e9e5cd18f
Michal Kodad <misa.kodad@gmail.com>
2022-07-14 19:58:17 +0200

    Added Shabby The Narwhal card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/ShabbyTheNarwhal.cs
@@ -0,0 +1,19 @@
+﻿using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class ShabbyTheNarwhal : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Shabby The Narwhal")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, you may search the deck for a Downgrade card and add it to your hand. Shuffle the deck.")
+                .Cast((Card owningCard) => new ActivatableEffect(owningCard,
+                        (_) => new AndEffect(owningCard,
+                            new SearchDeckEffect(owningCard, 1, CardLocation.Pile, (Card card) => card.CardType == ECardType.Downgrade),
+                            new ShuffleDeckEffect(owningCard)
+                        )
+                    )
+                );
+        }
+    }
+}
