dc3ca8fedf3bfb988b56a346c27bfdae4fe5be23
Michal Kodad <misa.kodad@gmail.com>
2022-07-15 01:11:16 +0200

    Added The Great Narwhal card + test

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/TheGreatNarwhal.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using UnstableUnicornCore.BasicEffects;
+
+[assembly: InternalsVisibleTo("UnstableUnicornCoreTest")]
+namespace UnstableUnicornCore.BaseSet {
+    public class TheGreatNarwhal : CardTemplateSource {
+        internal static Regex narwhalRegex = new Regex(@"\bnarwhal\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("The Great Narwhal")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, you may search the deck for a card with \"Narwhal\" in its name and add it to your hand. Shuffle the deck.")
+                .Cast((Card owningCard) => new ActivatableEffect(owningCard,
+                        new SearchDeckEffect(owningCard, 1, CardLocation.Pile, card => narwhalRegex.IsMatch(card.Name))
+                    )
+                );
+        }
+    }
+}
---
+++ UnstableUnicorn/UnstableUnicornCoreTest/BaseSet/TheGreatNarwhalTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore;
+using UnstableUnicornCore.BaseSet;
+using Xunit;
+
+namespace UnstableUnicornCoreTest.BaseSet {
+    public class TheGreatNarwhalTest {
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void TestRegex(CardTemplateSource card, bool shouldMatch) {
+            string name = card.GetCardTemplate().CreateCard().Name;
+            Assert.Equal(shouldMatch, TheGreatNarwhal.narwhalRegex.IsMatch(name));
+        }
+
+        public static IEnumerable<object[]> Data() {
+            yield return new object[] { new AlluringNarwhal(), true };
+            yield return new object[] { new ClassyNarwhal(), true };
+            yield return new object[] { new NarwhalTorpedo(), true };
+            yield return new object[] { new ShabbyTheNarwhal(), true };
+            yield return new object[] { new TheGreatNarwhal(), true };
+            yield return new object[] { new Americorn(), false };
+            yield return new object[] { new BrokenStable(), false };
+            yield return new object[] { new QueenBeeUnicorn(), false };
+            yield return new object[] { new SummoningRitual(), false };
+        }
+    }
+}
