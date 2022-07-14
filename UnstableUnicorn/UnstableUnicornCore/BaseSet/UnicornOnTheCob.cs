51826f640dce3e8b59e5adfc9b180095affeafc8
Michal Kodad <misa.kodad@gmail.com>
2022-07-15 01:59:24 +0200

    Added Unicorn On The Cob card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/UnicornOnTheCob.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class UnicornOnTheCob : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Unicorn On The Cob")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("When this card enters your Stable, DRAW 2 cards and DISCARD a card.")
+                .TriggerEffect(
+                    TriggerPredicates.WhenThisCardEntersYourStable,
+                    new List<ETriggerSource> { ETriggerSource.CardEnteredStable },
+                    (Card owningCard) => new AndEffect(owningCard,
+                        new DrawEffect(owningCard, 2),
+                        new DiscardEffect(owningCard, 1, ECardTypeUtils.CardTarget, PlayerTargeting.PlayerOwner)
+                    )
+                );
+        }
+    }
+}
