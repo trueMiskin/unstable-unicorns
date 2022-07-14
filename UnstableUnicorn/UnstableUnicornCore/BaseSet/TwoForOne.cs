067256737e0f4fb7c5cffbf3c487860973050db3
Michal Kodad <misa.kodad@gmail.com>
2022-07-15 01:33:46 +0200

    Added Two-for-one card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/TwoForOne.cs
@@ -0,0 +1,17 @@
+﻿using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class TwoForOne : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return Card
+                .Name("Two-for-one")
+                .CardType(ECardType.Spell)
+                .Text("SACRIFICE a card, then DESTROY 2 cards.")
+                .Cast((Card owningCard) => new ConditionalEffect(owningCard,
+                        new SacrificeEffect(owningCard, 1, ECardTypeUtils.CardTarget),
+                        new DestroyEffect(owningCard, 2, ECardTypeUtils.CardTarget)
+                    )
+                );
+        }
+    }
+}
