3993110f9329d90c5b2b8df4727e52c04076c8cb
Michal Kodad <misa.kodad@gmail.com>
2022-07-27 22:20:29 +0200

    Added class Deck and added deck for Second Print Base Set

---
+++ UnstableUnicorn/UnstableUnicornCore/Deck.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnstableUnicornCore {
+    public abstract class Deck {
+        public abstract IEnumerable<(CardTemplateSource card, int count)> BabyUnicorns();
+
+        public abstract IEnumerable<(CardTemplateSource card, int count)> OtherCards();
+    }
+}
