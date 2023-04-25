bdc64154326098f60b23f80af6ec39e7060eab79
Michal Kodad <misa.kodad@gmail.com>
2023-04-25 11:27:53 +0200

    Added Angel Unicorn card

---
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/AngelUnicorn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnstableUnicornCore.BasicEffects;
+
+namespace UnstableUnicornCore.BaseSet {
+    public class AngelUnicorn : CardTemplateSource {
+        public override CardTemplate GetCardTemplate() {
+            return EmptyCard
+                .Name("Angel Unicorn")
+                .CardType(ECardType.MagicUnicorn)
+                .Text("If this card is in your Stable at the beginning of your turn, you may SACRIFICE this card. If you do, choose a Unicorn card from the discard pile and bring it directly into your Stable.")
+                .TriggerEffect(
+                    TriggerPredicates.IsItInYourStableAtTheBeginningOfYourTurn,
+                    new List<ETriggerSource> { ETriggerSource.BeginningTurn },
+                    (Card owningCard) => new ActivatableEffect(owningCard,
+                        new ConditionalEffect(owningCard,
+                            new SacrificeThisCardEffect(owningCard),
+                            new BringCardFromSourceOnTable(owningCard, 1, card => ECardTypeUtils.UnicornTarget.Contains(card.CardType), CardLocation.DiscardPile)
+                        )
+                    )
+                );
+        }
+    }
+}
---
+++ UnstableUnicorn/UnstableUnicornCore/BasicEffects/SacrificeThisCardEffect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace UnstableUnicornCore.BasicEffects {
+    public sealed class SacrificeThisCardEffect : SacrificeEffect {
+        public SacrificeThisCardEffect(Card owningCard) : base(owningCard, 1, ECardTypeUtils.CardTarget) {}
+
+        public override void ChooseTargets(GameController gameController) {
+            CardTargets.Add(OwningCard);
+            CheckAndUpdateSelectionInActualLink(new List<Card>(), CardTargets, gameController);
+        }
+    }
+}
