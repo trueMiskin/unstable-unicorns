2e381d4b31d48058c108ebec6540351b93c403f3
Michal Kodad <misa.kodad@gmail.com>
2023-04-27 23:42:01 +0200

    Changed Shark with a horn calls a different (non-reacting) effect

--- UnstableUnicorn/UnstableUnicornCore/BaseSet/SharkWithAHorn.cs
+++ UnstableUnicorn/UnstableUnicornCore/BaseSet/SharkWithAHorn.cs
@@ -8,9 +8,8 @@ namespace UnstableUnicornCore.BaseSet {
                 .CardType(ECardType.MagicUnicorn)
                 .Text("When this card enters your Stable, you may SACRIFICE this card. If you do, DESTROY a Unicorn card.")
                 .Cast((Card owningCard) => new ActivatableEffect(owningCard,
-                        // if done sacrifice then destroy, TODO:  maybe change implementation of ConditionalEffect
                         new ConditionalEffect(owningCard,
-                            new SacrificeThisCardInsteadOtherCard(owningCard, ECardTypeUtils.CardTarget /* */),
+                            new SacrificeThisCardEffect(owningCard),
                             new DestroyEffect(owningCard, 1, ECardTypeUtils.UnicornTarget)
                         )
                     )
