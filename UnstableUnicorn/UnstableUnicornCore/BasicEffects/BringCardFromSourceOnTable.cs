1dd12b3001ff1f9231822f88f72b04291ac653be
Michal Kodad <misa.kodad@gmail.com>
2023-04-27 23:31:27 +0200

    Don't bring cards (bring effect) that are already targeted

--- UnstableUnicorn/UnstableUnicornCore/BasicEffects/BringCardFromSourceOnTable.cs
+++ UnstableUnicorn/UnstableUnicornCore/BasicEffects/BringCardFromSourceOnTable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UnstableUnicornCore.BasicEffects {
     public class BringCardFromSourceOnTable : AEffect {
@@ -24,12 +25,14 @@ namespace UnstableUnicornCore.BasicEffects {
             };
 
             var cards = cardsSource.FindAll(allowedCardsPredicate);
+            cards = RemoveCardsWhichAreTargeted(cards, gameController);
 
             CardCount = Math.Min(CardCount, cards.Count);
             // maybe a little bit wrong used this method for selection
             CardTargets = OwningPlayer.WhichCardsToGet(CardCount, this, cards);
 
             ValidatePlayerSelection(CardCount, CardTargets, cards);
+            CheckAndUpdateSelectionInActualLink(new List<Card>(), CardTargets, gameController);
         }
 
         public override void InvokeEffect(GameController gameController) {
