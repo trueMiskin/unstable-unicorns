b58dc0c0adc06af06298d4cf6eda3f6dc1d870fa
Michal Kodad <misa.kodad@gmail.com>
2023-06-28 11:35:14 +0200

    Added license header

--- UnstableUnicorn/UnstableUnicornCore/BasicEffects/DrawEffect.cs
+++ UnstableUnicorn/UnstableUnicornCore/BasicEffects/DrawEffect.cs
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 ﻿namespace UnstableUnicornCore.BasicEffects {
     public sealed class DrawEffect : AEffect {
         public DrawEffect(Card owningCard, int cardCount) : base(owningCard, cardCount) {}
